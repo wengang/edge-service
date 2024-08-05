d6557f655bccc80533cfd2c9dfe7157d5efe235f
kang <tao-yu@hotmail.com>
2024-08-05 15:47:02 +0800

    chapter 09

---
+++ src/main/java/com/polarbookshop/edgesrvice/EdgeSrviceApplication.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.edgesrvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeSrviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeSrviceApplication.class, args);
+	}
+
+}
